712fd45c59050b8e32a116cfc589605364108ff6
poooppppppp <91991093+QuardiGras@users.noreply.github.com>
2021-10-19 21:08:01 +0000

    Added percent of next cata level is complete

--- checker.py
+++ checker.py
@@ -13,10 +13,17 @@ api0 = requests.get('https://sky.shiiyu.moe/api/v2/dungeons/' + ign + '/' + prof
 
 invalid = 'error' in api0
 
+progress = api0['dungeons']['catacombs']['level']['progress']
+
+progress_roundable = progress * 100
+
+progress_printable = round(progress_roundable, 2)
+
 if invalid == True:
   print('Invalid inputs, try again.')
 else:
-  print('Catacombs level for user "' + ign + '" on the profile "' + profile + '": ', api0['dungeons']['catacombs']['level']['level'])
+  print('Catacombs level for user "' + ign + '" on the profile "' + profile + '":', api0['dungeons']['catacombs']['level']['level'])
+  print('Estimated percent of the way there to the next catacombs level:', progress_printable)
 
 
 # ['dungeons']['catacombs']['level']['level']
