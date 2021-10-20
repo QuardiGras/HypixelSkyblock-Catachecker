550bca7969b54bb4f465cf6c7739d3651c7c245b
poooppppppp <91991093+QuardiGras@users.noreply.github.com>
2021-10-20 15:49:34 +0000

    Added more error handling

--- checker.py
+++ checker.py
@@ -14,17 +14,21 @@ api0 = requests.get('https://sky.shiiyu.moe/api/v2/dungeons/' + ign + '/' + prof
 invalid = 'error' in api0
 
 if invalid == True:
-  print('Invalid inputs, try again.')
-else:
+  print(' ')
+else: 
+  print('Catacombs level for user "' + ign + '" on the profile "' + profile + '": ', api0['dungeons']['catacombs']['level']['level'])
+
+
+# ['dungeons']['catacombs']['level']['level']
+
+try:
   progress = api0['dungeons']['catacombs']['level']['progress']
 
   progress_roundable = progress * 100
 
   progress_printable = round(progress_roundable, 2)
-  
-  print('Catacombs level for user "' + ign + '" on the profile "' + profile + '": ', api0['dungeons']['catacombs']['level']['level'])
-  
+
   print('Estimated percent of the way there to the next catacombs level:', progress_printable)
 
-
-# ['dungeons']['catacombs']['level']['level']
+except:
+  print('Invalid inputs, try again.')
