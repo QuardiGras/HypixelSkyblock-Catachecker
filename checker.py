1b5248fbbef00cc50dacf369eb58332937125075
poooppppppp <91991093+QuardiGras@users.noreply.github.com>
2021-10-25 17:15:55 +0000

    Formatting

--- checker.py
+++ checker.py
@@ -1,28 +1,31 @@
-
 # importing dependencies
 import requests
 
 # defining functions
 
-# deciding 
+# deciding
 ign = input('What is your ign?\n')
 profile = input('What profile?\n')
 
 # main
-api0 = requests.get('https://sky.shiiyu.moe/api/v2/dungeons/' + ign + '/' + profile).json()
+api0 = requests.get('https://sky.shiiyu.moe/api/v2/dungeons/' + ign + '/' +
+                    profile).json()
 
 try:
-  progress = api0['dungeons']['catacombs']['level']['progress']
+    progress = api0['dungeons']['catacombs']['level']['progress']
 
-  progress_roundable = progress * 100
+    progress_roundable = progress * 100
 
-  progress_printable = round(progress_roundable, 2)
+    progress_printable = round(progress_roundable, 2)
 
-  print('Estimated percent of the way there to the next catacombs level:', progress_printable)
-  
-  print('Catacombs level for user "' + ign + '" on the profile "' + profile + '": ', api0['dungeons']['catacombs']['level']['level'])
+    print('Estimated percent of the way there to the next catacombs level:',
+          progress_printable)
+
+    print(
+        'Catacombs level for user "' + ign + '" on the profile "' + profile +
+        '": ', api0['dungeons']['catacombs']['level']['level'])
 
 except:
-  print('Invalid inputs, try again.')
+    print('Invalid inputs, try again.')
 
 # ['dungeons']['catacombs']['level']['level']
